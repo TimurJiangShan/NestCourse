d721013a9926e1a956bb2b18e58d289485e35841
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-02-03 15:19:50 +1100

    Add 7.16 solution

--- 7.16- Exercise/solution.js
+++ 7.16- Exercise/solution.js
@@ -1,32 +1,29 @@
 const mongoose = require("mongoose");
+
 mongoose.connect("mongodb://localhost/mongo-exercises")
-  .then(() => console.log("Connect to MongoDB"))
-  .catch(err => console.log("Could not connect to MongoDB"));
+  .then(() => console.log("Connect to MongoDB..."))
+  .catch(err => console.error("Could not connect to MongoDB"));
 
 const courseSchema = new mongoose.Schema({
   name: String,
   author: String,
   tags: [String],
-  date: { type: Date, default: Date.now },
+  date: { type: Date, default: Date.now},
   isPublished: Boolean,
   price: Number,
-});
+})
 
 const Course = mongoose.model("Course", courseSchema);
 
 async function getCourses() {
   const courses = await Course
-    .find({ isPublished: true, tags: { $in: ["frontend","backend"] }})
-    .sort({ price: -1})
-    .select({ name: 1, author: 1});
-
+    .find({ isPublished: true})
+    .or([{ price: { $gte: 15 }}, { name: /.*by.*/ }])
   return courses;
 }
 
 async function run() {
-  const result = await getCourses();
-  console.log(result);
+ console.log(await getCourses());
 }
-
 
 run();
