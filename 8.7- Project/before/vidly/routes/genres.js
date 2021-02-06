9c49ecf29e4624af05a58b662f9c9c7e788df970
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-02-06 14:02:45 +1100

    feat: add referencing document

--- 8.7- Project/before/vidly/routes/genres.js
+++ 8.7- Project/before/vidly/routes/genres.js
@@ -1,47 +1,6 @@
 const express = require('express');
-const Joi = require('joi');
 const router = express.Router();
-
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost/vidly")
-  .then(() => console.log("Connect to Vidly database"))
-  .catch(err => console.error("Could not connect to Vidly MongoDB"));
-
-const genreSchema = new mongoose.Schema({
-  name: { type: String, required: true},
-})
-
-const Genre = mongoose.model("Genre", genreSchema);
-
-async function getAllGenres() {
-  const genres = await Genre.find();
-  return genres;
-}
-
-async function getGenre(id) {
-  const genre = await Genre.findById(id);
-  console.log(id);
-  return genre;
-}
-
-async function createGenre(name) {
-  const genre = new Genre({
-    name: name
-  })
-  const result = await genre.save();
-  return result;
-}
-
-async function updateGenre(id, name) {
-  const genre = await Genre.findByIdAndUpdate(id, { name: name }, { new: true});
-  return genre;
-}
-
-async function removeGenre(id) {
-  const result = Genre.deleteOne({ _id: id});
-  return result;
-}
+const { Genre, getAllGenres, getGenre, createGenre, updateGenre, removeGenre } = require('../models/genre');
 
 /***************************************************/
 router.get('/', (req, res) => {
@@ -77,12 +36,5 @@ router.get('/:id', (req, res) => {
     .catch(error => res.status(404).send('The genre with the given ID was not found.'));
 });
 
-function validateGenre(genre) {
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-
-  return Joi.validate(genre, schema);
-}
 
 module.exports = router;
