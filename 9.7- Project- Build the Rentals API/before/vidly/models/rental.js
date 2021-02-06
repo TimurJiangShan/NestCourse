86b0fbe79e288df0c824cc842191149df3251564
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-02-06 23:06:11 +1100

    feat: add joi-objectid validation

--- 9.7- Project- Build the Rentals API/before/vidly/models/rental.js
+++ 9.7- Project- Build the Rentals API/before/vidly/models/rental.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
+Joi.objectId = require('joi-objectid')(Joi);
 const customerSchema = require('./customer');
 
 const Rental = mongoose.model(
@@ -40,10 +41,12 @@ const Rental = mongoose.model(
     },
   }),
 );
+// 用 joi-objectid这个包来验证 objectId是不是有效的ID. 这样当前端传过来非法的id的时候， 后端的console不会报错，
+// -- 会向前端发送一个400 Bad Request。 如果不用objectid验证， 当前端发送非法的id的时候，后端会报unhandleedPromise的错误
 const validateRental = (rental) => {
   const schema = {
-    customerId: Joi.string().required(),
-    movieId: Joi.string().required(),
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required(),
   };
   return Joi.validate(rental, schema);
 };
