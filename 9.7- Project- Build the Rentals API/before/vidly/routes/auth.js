cc6589c8bd4cbff63e881597049fb15c2813fa57
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-02-07 13:17:41 +1100

    feat: auth post

---
+++ 9.7- Project- Build the Rentals API/before/vidly/routes/auth.js
@@ -0,0 +1,32 @@
+const _ = require('lodash');
+const { User } = require('../models/user');
+const mongoose = require('mongoose');
+const express = require('express');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('Invalid user or password');
+
+  /*********** 密码验证： 给req的password加盐，然后和数据库里的password进行比较 *************/
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send('Invalid user or password');
+  /*********************************/
+
+  res.send(true);
+});
+
+function validate(req) {
+  const schema = {
+    name: Joi.string().min(3).max(50).required(),
+    password: Joi.string().min(5).max(255).required(),
+  };
+
+  return Joi.validate(req, schema);
+}
+
+module.exports = router;
