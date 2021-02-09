d1bbdba49d20b2ab3af35a835ba1339b12d7e2b0
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-02-09 23:13:03 +1100

    feat: error handling middleware

--- 9.7- Project- Build the Rentals API/before/vidly/index.js
+++ 9.7- Project- Build the Rentals API/before/vidly/index.js
@@ -5,6 +5,7 @@ const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
+const error = require('./middleware/error');
 const express = require('express');
 const app = express();
 const Joi = require('joi');
@@ -29,6 +30,9 @@ app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
+
+// Error handling 的function在其他middleware function的后面, 所以在其他middleware里面，当错误出现的时候，调用next(), 就会被这个error handling的middleware所捕捉
+app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- 9.7- Project- Build the Rentals API/before/vidly/routes/genres.js
+++ 9.7- Project- Build the Rentals API/before/vidly/routes/genres.js
@@ -1,28 +1,36 @@
 const { Genre, validate } = require('../models/genre');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
-const mongoose = require('mongoose');
+const asyncMiddleware = require('../middleware/async');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  try {
+// router.get('/', async (req, res) => {
+//   const genres = await Genre.find().sort('name');
+//   res.send(genres);
+// });
+
+router.get(
+  '/',
+  asyncMiddleware(async (req, res) => {
     const genres = await Genre.find().sort('name');
     res.send(genres);
-  } catch (error) {
-    res.status(500).send('Something failed');
-  }
-});
+  }),
+);
 
-router.post('/', auth, async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.post(
+  '/',
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-  const genre = new Genre({ name: req.body.name });
-  await genre.save();
+    const genre = new Genre({ name: req.body.name });
+    await genre.save();
 
-  res.send(genre);
-});
+    res.send(genre);
+  }),
+);
 
 router.put('/:id', auth, async (req, res) => {
   const { error } = validate(req.body);
